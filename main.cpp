119eee17d58f9f1175da510906c2171a7b6c566e
Alexcyfy <acollanton@mail.csuchico.edu>
2018-09-19 16:24:09 -0700

    Takes firstname and lastname, makes initials, and tells you your lucky number based off length of firstname

--- main.cpp
+++ main.cpp
@@ -9,14 +9,17 @@ int main()
   //declare variables
   string firstname;
   string lastname;
+  int lucky;
   //get user input
   cout<<"What is your first name? ";
   cin>>firstname;
   cout<<"What is your last name? ";
   cin>>lastname;
   //tell fortune
+  lucky = firstname.length();
   cout<<"Welcome, ";
   cout<<firstname[0]<<lastname[0];
-  cout<<", here is your fortune..."<<endl;
+  cout<<", here is your fortune...";
+  cout<<"Your lucky number is "<<lucky<<endl;
   return 0;
 }
