eed4e9f534da05f90f484e89f0d553e6c2c04115
Alexcyfy <acollanton@mail.csuchico.edu>
2018-09-24 16:23:26 -0700

    Now tells if famous, or low profile

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@ int main()
   //declare variables
   string firstname;
   string lastname;
+  int index = firstname.length() -1;
   int lucky;
   //get user input
   cout<<"What is your first name? ";
@@ -21,5 +22,14 @@ int main()
   cout<<firstname[0]<<lastname[0];
   cout<<", here is your fortune...";
   cout<<"Your lucky number is "<<lucky<<endl;
+  if(firstname[0] == 'A' || firstname[0] == 'E' || firstname[0] == 'I' || firstname[0] == 'O' || firstname[0]=='U'||
+firstname[0]=='a'||firstname[0]=='e'|| firstname[0]=='i'||firstname[0]=='o'||firstname[0]=='u')    
+  {
+  cout<<"You are destined to be famous!";
+  }
+  else
+  {
+  cout<<"You should keep a low profile.";
+  }
   return 0;
 }
